2dc83767ea5d142e229a3428cd547a7f4d46df9b
Nachum Schwartz <nschwartz814@gmail.com>
2024-06-06 23:54:46 -0400

    GamePanel first commit

---
+++ GamePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel{
+	static CardFace[] playerCards, dealerCards;
+	static CardBack deckImage;
+	private static Deck deck;
+	private static JButton dealButton, hitButton, standButton;
+	private static JLabel winsLabel, lossesLabel, tiesLabel, winsVariableLabel, lossesVariableLabel, tiesVariableLabel;
+	private static Menu menu; 
+
+	public GamePanel() {
+
+	}
+}
