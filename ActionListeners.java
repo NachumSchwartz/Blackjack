a0dfdc7f29d1fe37478799fb74b5fa8b7d8d3d53
Nachum Schwartz <nschwartz814@gmail.com>
2024-06-09 17:57:25 -0400

    Game Menu Action Listener completed

--- ActionListeners.java
+++ ActionListeners.java
@@ -1,24 +1,36 @@
 import java.awt.event.*;
+import javax.swing.JComboBox;
+
 class GameMenuAL implements ActionListener{
-    public void actionPerformed(ActionEvent e){  
-         
+    JComboBox<String> menu;
+    
+    public GameMenuAL(JComboBox<String> menu){
+        this.menu = menu;
+    }
+    public void actionPerformed(ActionEvent e){
+        String choice = (String) menu.getSelectedItem();
+        GameStart.choiceSwitch(choice);
     }
 }
+
 class DealButtonAL implements ActionListener{
     public void actionPerformed(ActionEvent e){  
          
     }
 }
+
 class HitButtonAL implements ActionListener{
     public void actionPerformed(ActionEvent e){  
          
     }
 }
+
 class StandButtonAL implements ActionListener{
     public void actionPerformed(ActionEvent e){  
          
     }
 }
+
 class HintButtonAL implements ActionListener{
     public void actionPerformed(ActionEvent e){  
          
