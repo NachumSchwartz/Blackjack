40955f7fe8e22750fb8925b50b183390c86c7de5
Nachum Schwartz <nschwartz814@gmail.com>
2024-06-09 18:00:26 -0400

    buttons added; menu added with ActionListener

--- GamePanel.java
+++ GamePanel.java
@@ -2,14 +2,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel{
-	static CardFace[] playerCards, dealerCards;
-	static CardBack deckImage;
+	private static CardFace[] playerCards, dealerCards;
+	private static CardBack deckImage;
 	private static Deck deck;
-	private static JButton dealButton, hitButton, standButton;
+	private static JButton dealButton, hitButton, standButton, hintButton;
 	private static JLabel winsLabel, lossesLabel, tiesLabel, winsVariableLabel, lossesVariableLabel, tiesVariableLabel;
 	private static Menu menu; 
+	private static GameMenuAL gmal;
+	private static DealButtonAL dealbal;
+	private static HitButtonAL hitbal;
+	private static StandButtonAL standbal;
+	private static HintButtonAL hintbal;
 
-	public GamePanel() {
-
+	public GamePanel(){
+		menu = new Menu();
+		gmal = new GameMenuAL(menu);
+		menu.addActionListener(gmal);
+		this.add(menu);
 	}
 }
