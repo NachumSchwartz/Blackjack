9957c89e0409958cc2797bbfc1021d5ed63af266
Nachum Schwartz <nschwartz814@gmail.com>
2024-06-09 18:02:18 -0400

    changed attributes

--- CardFace.java
+++ CardFace.java
@@ -10,12 +10,16 @@ public class CardFace extends JPanel{
     private String suit;
     private Color color;
 
-    public CardFace(String value, String suit, Color color){
+    public CardFace(String value, String suit){
         this.value = value;
         this.suit = suit;
-        this.color = color;
+        if(suit.equals("Hearts")||suit.equals("Diamonds")){
+            this.color = Color.red;
+        }else if(suit.equals("Spades")||suit.equals("Clubs")){
+            this.color = Color.black;
+        }
 
-        Border colorBorder = BorderFactory.createMatteBorder(10, 10, 10, 10, color);
+        Border colorBorder = BorderFactory.createMatteBorder(10, 10, 10, 10, this.color);
         Border emptyBorder = BorderFactory.createEmptyBorder(2,2,2,2);
         Border compoundOutside = BorderFactory.createCompoundBorder(colorBorder, emptyBorder);
         Border wholeBorder = BorderFactory.createCompoundBorder(compoundOutside, colorBorder);
@@ -25,7 +29,7 @@ public class CardFace extends JPanel{
         setBackground(Color.white);
         setLayout(new GridBagLayout());
         
-        Emblem emblem = new Emblem(value, suit, color); 
+        Emblem emblem = new Emblem(value, suit, this.color); 
     
         add(emblem); 
     }
