5fae337c4c82cc1847445502c1df858ad16a699f
Nachum Schwartz <nschwartz814@gmail.com>
2024-06-10 00:18:02 -0400

    add deal button

--- GamePanel.java
+++ GamePanel.java
@@ -4,10 +4,11 @@ import java.awt.*;
 public class GamePanel extends JPanel{
 	private static CardFace[] playerCards, dealerCards;
 	private static CardBack deckImage;
-	private static Deck deck;
 	private static JButton dealButton, hitButton, standButton, hintButton;
 	private static JLabel winsLabel, lossesLabel, tiesLabel, winsVariableLabel, lossesVariableLabel, tiesVariableLabel;
 	private static Menu menu; 
+
+	//ActionListeners
 	private static GameMenuAL gmal;
 	private static DealButtonAL dealbal;
 	private static HitButtonAL hitbal;
@@ -19,5 +20,8 @@ public class GamePanel extends JPanel{
 		gmal = new GameMenuAL(menu);
 		menu.addActionListener(gmal);
 		this.add(menu);
+		dealButton = new JButton("Deal");
+		dealButton.addActionListener(dealbal);
+		this.add(dealButton);
 	}
 }
